18c70646132131a605971f7208fab9cb72e56a2e
Leah Boyd <leahboyd08@gmail.com>
2019-03-16 14:33:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
-  if int % 5 == 0
+  elsif int % 5 == 0
     return "Buzz"
-  if int % 3 && 5 == 0
+  elsif int % 3 && 5 == 0
     return "Fizzbuzz"
-  if int !% 3 || 5 == 0
+  else
     return "nil"
   end
 end
